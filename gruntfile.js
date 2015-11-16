3ab8518a9e3523484213957acdb1adab58ac4084
Hyunsang Sun <asunhs@gmail.com>
2015-11-16 15:11:18 +0900

    templates and scss setup

--- gruntfile.js
+++ gruntfile.js
@@ -8,17 +8,21 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-env');
     grunt.loadNpmTasks('grunt-html2js');
+    grunt.loadNpmTasks('grunt-sass');
 
     // Default tasks.
     grunt.registerTask('lib', ['copy']);
-    grunt.registerTask('default', ['lib', 'browserify', 'uglify', 'html2js']);
+    grunt.registerTask('app', ['browserify', 'html2js', 'sass']);
+    grunt.registerTask('default', ['lib', 'app', 'uglify']);
 
     grunt.initConfig({
         baseDir: 'src',
         distDir: 'dist',
         pkg: grunt.file.readJSON('package.json'),
         src: {
-            js: ['<%=baseDir%>/app/**/*.js']
+            js: ['<%=baseDir%>/app/**/*.js'],
+            templates: ['<%= baseDir %>/app/**/*.tpl.html'],
+            sass: ['<%= baseDir %>/styles/main.scss']
         },
         uglify: {
             options: {
@@ -34,20 +38,27 @@ module.exports = function(grunt) {
             }
         },
         browserify: {
-            build: {
+            js: {
                 options: {
                     debug: false,
                     transform: ['browserify-ngannotate']
                 },
                 files: {
-                    'dist/app.js': ['<%=baseDir%>/**/*.js']
+                    '<%= distDir %>/app.js': ['<%= src.js %>']
                 }
             }
         },
         html2js: {
             main: {
-                src: ['<%=baseDir%>/**/*.tpl.html'],
+                src: ['<%= src.templates %>'],
                 dest: '<%= distDir %>/templates.js'
+            }
+        },
+        sass: {
+            dist: {
+                files: {
+                    '<%= distDir %>/main.css': '<%= src.sass %>'
+                }
             }
         },
         copy: {
