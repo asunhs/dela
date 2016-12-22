779bc76c9f206debb57b81f337e290519c9a2d8a
Sun <hyunsang.sun@samsung.com>
2016-12-22 20:54:33 +0900

    opened

--- app.js
+++ app.js
@@ -180,8 +180,9 @@ function DelaSvc(JSONPSvc, CountSvc, Cards, StoreSvc, FirebaseSvc) {
     function getMenus() {
         // return JSONPSvc.request(DELA_URL + 'menus').then(afterGetMenus);
         return FirebaseSvc.getJamsilMenu().then(function (data) {
-            Cards.list = data.menus;
+            Cards.list = data.menus || [];
             Cards.time = data.time;
+            Cards.opened = data.opened;
 
             return Cards;
         });
@@ -227,6 +228,7 @@ function DelaCtrl($document, $scope, DelaSvc, YahooWeatherAPI, StockSvc, Firebas
     DelaSvc.getMenus().then(function (cards) {
         $scope.menus = cards.list;
         $scope.time = cards.time;
+        $scope.opened = cards.opened;
     });
 
     $scope.orderFactor = ['', 'cal', 'price'];
